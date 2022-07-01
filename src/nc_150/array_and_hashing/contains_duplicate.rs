845d3e71dfe2578f6e057d27291e0eb05d1e2065
Luke Eltiste <luke343279@gmail.com>
2022-07-01 11:34:28 -0700

    Array & Hashing #1 - Contains Duplicate

---
+++ src/nc_150/array_and_hashing/contains_duplicate.rs
@@ -0,0 +1,37 @@
+use std::collections::HashSet;
+
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut seen = HashSet::new();
+
+    for num in nums {
+        if seen.contains(&num) {
+            return true;
+        }
+        seen.insert(num);
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let result = contains_duplicate(vec![1, 2, 3, 1]);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn example_2() {
+        let result = contains_duplicate(vec![1, 2, 3, 4]);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn example_3() {
+        let result = contains_duplicate(vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2]);
+        assert_eq!(result, true);
+    }
+}
