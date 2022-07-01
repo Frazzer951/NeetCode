bfc86ff91334ec944b34c60496d064ebc67892c8
Luke Eltiste <luke343279@gmail.com>
2022-07-01 12:02:20 -0700

    Array & Hashing #2 - Valid Anagram

---
+++ src/nc_150/array_and_hashing/valid_anagram.rs
@@ -0,0 +1,27 @@
+pub fn is_anagram(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+
+    let mut chars = std::collections::HashMap::new();
+    s.chars().for_each(|c| *chars.entry(c).or_insert(0) += 1);
+    t.chars().for_each(|c| *chars.entry(c).or_insert(0) -= 1);
+    chars.into_values().all(|v| v == 0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let result = is_anagram("anagram".to_string(), "nagaram".to_string());
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn example_2() {
+        let result = is_anagram("rat".to_string(), "car".to_string());
+        assert_eq!(result, false);
+    }
+}
