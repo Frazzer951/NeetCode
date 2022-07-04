87fdf0f6a9a7474a3ba79cc8a40ce712eb453a53
Luke Eltiste <luke343279@gmail.com>
2022-07-04 15:49:41 -0700

    Add comments explaining problems

--- src/nc_150/array_and_hashing/contains_duplicate.rs
+++ src/nc_150/array_and_hashing/contains_duplicate.rs
@@ -1,3 +1,4 @@
+/// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 use std::collections::HashSet;
 
 pub fn contains_duplicate(nums: Vec<i32>) -> bool {
