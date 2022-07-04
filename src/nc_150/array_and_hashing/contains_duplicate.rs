a58f4284665e84db3aeca505c8878982bbe9ccbd
Luke Eltiste <luke343279@gmail.com>
2022-07-04 11:42:35 -0700

    make public

--- src/nc_150/array_and_hashing/contains_duplicate.rs
+++ src/nc_150/array_and_hashing/contains_duplicate.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-fn contains_duplicate(nums: Vec<i32>) -> bool {
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
     let mut seen = HashSet::new();
 
     for num in nums {
--- src/nc_150/array_and_hashing/two_sum.rs
+++ src/nc_150/array_and_hashing/two_sum.rs
@@ -1,4 +1,4 @@
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     for i in 0..nums.len() - 1 {
         for j in i + 1..nums.len() {
             if nums[i] + nums[j] == target {
