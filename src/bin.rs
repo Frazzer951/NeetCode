3397e773c7a75ab2add76a0268d32d8c338b5e1b
Luke Eltiste <luke343279@gmail.com>
2023-02-16 16:10:32 -0800

    cleanup

--- src/bin.rs
+++ src/bin.rs
@@ -2,5 +2,5 @@ use neet_code::nc_150::two_pointer::valid_palindrome::is_palindrome;
 
 fn main() {
     let result = is_palindrome("a.".to_string());
-    println!("{:#?}", result);
+    println!("{result:#?}");
 }
